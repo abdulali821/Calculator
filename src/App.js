77afa6f65c22c868c7733326fc53de78521cc81c
abdulali821 <abdulali821@users.noreply.github.com>
2022-03-28 23:44:53 +0500

    Comit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { useState } from "react";
+import image from "./img/backImg.jpg";
+
 function App() {
-  
+  const [calc, setCalc] = useState("");
+  const [result, setResult] = useState("");
+
+  const ops = ['/','*','-','+','.'];
+
+  const updateCal = value => {
+    setCalc(calc+value);
+  }
+
   const numbers=()=>{
     const no=[];
     for (let i = 1; i < 10; i++) {
@@ -10,25 +21,37 @@ function App() {
     return no;
   }
   return (
-    <div className="App">
+    <div 
+    style={{ backgroundImage:`url(${image})` }}
+    className="App">
+      <div className="title">
+        <h1>
+          CALCULATOR
+        </h1>
+        </div>
       <div className="calculator">
         <div className="display">
-          <span>(0) </span>0
+         {result? <span>(0) </span> : ""} {calc || "0"}
         </div>
           <div className="operators">
-            <button>/</button>
-            <button>+</button>
-            <button>-</button>
-            <button>*</button>
-            <button>DEL</button>
+            <button onClick={()=>{updateCal("/")}}>/</button>
+            <button onClick={()=>{updateCal("+")}}>+</button>
+            <button onClick={()=>{updateCal("-")}}>-</button>
+            <button onClick={()=>{updateCal("*")}}>*</button>
+            <button >DEL</button>
           </div>
           <div className="digit">
             {numbers()}
-            <button>0</button>
-            <button>.</button>
+            <button onClick={()=>{updateCal("0")}}>0</button>
+            <button onClick={()=>{updateCal(".")}}>.</button>
             <button>=</button>
           </div>
       </div>
+      <div className="landing">
+        <h2>
+          Made by Me❤️
+        </h2>
+        </div>
     </div>
   );
 }
