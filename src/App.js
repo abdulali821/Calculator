cb0d380b20b24423cd14a197550ef76aa0cb2607
abdulali821 <abdulali821@users.noreply.github.com>
2022-03-29 00:50:10 +0500

    comit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,28 @@ import image from "./img/backImg.jpg";
 function App() {
   const [calc, setCalc] = useState("");
   const [result, setResult] = useState("");
-
   const ops = ['/','*','-','+','.'];
 
   const updateCal = value => {
+    if((ops.includes(value) && calc=== '') || (ops.includes(value) && ops.includes(calc.slice(-1))))
+    {
+      return;
+    }
     setCalc(calc+value);
+    if(!ops.includes(value)){
+      setResult(eval(calc+value).toString());
+    }
   }
-
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  }
+  const deleteLast = () =>{
+    if(calc===''){
+      return;
+    }
+    const value = calc.slice(0 ,-1);
+    setCalc(value);
+  }
   const numbers=()=>{
     const no=[];
     for (let i = 1; i < 10; i++) {
@@ -31,20 +46,20 @@ function App() {
         </div>
       <div className="calculator">
         <div className="display">
-         {0? <span>(0) </span> : ""} {calc || "0"}
+         {result? <span>({result}) </span> : ""} {calc || "0"}
         </div>
           <div className="operators">
             <button onClick={()=>{updateCal("/")}}>/</button>
             <button onClick={()=>{updateCal("+")}}>+</button>
             <button onClick={()=>{updateCal("-")}}>-</button>
             <button onClick={()=>{updateCal("*")}}>*</button>
-            <button >DEL</button>
+            <button onClick={deleteLast}>DEL</button>
           </div>
           <div className="digit">
             {numbers()}
             <button onClick={()=>{updateCal("0")}}>0</button>
             <button onClick={()=>{updateCal(".")}}>.</button>
-            <button>=</button>
+            <button onClick={calculate}>=</button>
           </div>
       </div>
       <div className="landing">
